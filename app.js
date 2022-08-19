3dcbf0527f95bf038c19767c75af507ee934aa86
yashiyoshi <93021275+yashiyoshi@users.noreply.github.com>
2022-08-19 16:41:57 +0300

    update

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 let form = document.getElementById("form");
 let input = document.getElementById("input");
-let msg = document.getElementById("msg");
+let msg = document.getElementById("passwordButtonMsg");
 let posts = document.getElementById("posts");
+
 
 form.addEventListener("submit", (e) => {
 
-    console.log("button clicked");
-  
-    formValidation();
-  });
+  formValidation();
+});
 
-  let formValidation = () => {
-    if (input.value === "") {
-      msg.innerHTML = "Post cannot be blank";
-      console.log("failure");
-    } else {
-      console.log("success");
-      msg.innerHTML = "";
-      acceptData();
+let formValidation = () => {
+  if (input.value === "") {
+    msg.innerHTML = "Post cannot be blank";
+  } else {
+    if(input.value == "yassir"){
+      window.location.replace("editTTD.html");
     }
-  };
+    else{
+      msg.innerHTML = "Wrong password!";
+    }
+  }
+};
+
+
+
+
 
 let data = [];
 
@@ -35,28 +40,4 @@ let acceptData = () => {
 
 
     createPost();
-  };
-
-  let createPost = () => {
-    posts.innerHTML += `
-    <div>
-    <p>${data.text}</p>
-    <span class="options">
-      <i onClick="editPost(this)" class="fas fa-edit"></i>
-      <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
-    </span>
-  </div>
-    `;
-    input.value = "";
-
-  };
-
-  let deletePost = (e) => {
-    e.parentElement.parentElement.remove();
-  };
-
-
-  let editPost = (e) => {
-    input.value = e.parentElement.previousElementSibling.innerHTML;
-    e.parentElement.parentElement.remove();
   };
